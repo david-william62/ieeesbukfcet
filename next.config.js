cfc9a1eded5848bc162e8686e7977a636cbefb12
David William <davidwilliam35302@gmail.com>
2025-09-09 00:09:37 +0530

    allowed cors

--- next.config.js
+++ next.config.js
@@ -4,7 +4,28 @@ const nextConfig = {
   swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Allow all origins or specify 'https://ieeesbukfcet.in'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
